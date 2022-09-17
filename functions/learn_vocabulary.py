3e5f91a5a8b18204b7d2600079e67909de2536a3
Dariusz_Potepa <dariusz.potepa29@gmail.com>
2022-09-17 11:43:04 +0200

    add new file to new function in app

---
+++ functions/learn_vocabulary.py
@@ -0,0 +1,5 @@
+# Steps to correct learn vocabulary
+# step1 = understand it (write defintions this word or phase)
+# step2 = pronounce it (slowly, loudly, clearly)
+# step3 = spell it (spell each letter)
+# step4 = recognize it
