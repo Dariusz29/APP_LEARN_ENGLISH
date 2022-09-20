927e5c47803622044150d4c9b7ac972ba670f973
Dariusz_Potepa <dariusz.potepa29@gmail.com>
2022-09-20 22:39:31 +0200

    add modified to new file

--- functions/save_results.py
+++ functions/save_results.py
@@ -1,23 +1,31 @@
 #saving history scores about my learn of vocabulary
 
 
-with open('results_history.txt', 'r') as rh:
-    rh = rh
 
 
-def save_results(result, results=rh):
 
-    if result == 'good':
-        results[len(results)+1] = 1
+def save_results(result_):
+
+    with open('results_history.txt', 'r') as rh:
+        results = list(rh)
+
+    if result_ == 'good':
+        print(len(results))
+        score  = 1
+        results.append(score)
     
-    elif result == 'half good':
-        results[len(results)+1] = 0.5
+    elif result_ == 'half good':
+        print(len(results))
+        score = 0.5
+        results.append(score)
 
     else:
-        results[len(results)+1] = 0
+        print(len(results))
+        score = 0
+        results.append(score)
 
     for w in results:
         rh.write(w)
         rh.write('\n')
 
-    rh.close
+    
